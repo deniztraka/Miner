51a0233f7efd3245f2c58aa467bea7f559fe7f3d
RELEPHANT\d.traka <d.traka@emakina.nl>
2017-11-08 14:06:28 +0300

    some improvements

--- src/states/Running.ts
+++ src/states/Running.ts
@@ -17,9 +17,10 @@ namespace Darkworld.States {
 
             this.game.worldMap = this.game.add.tilemap(null, 16, 16, this.mapWidth, this.mapHeight) as Darkworld.Core.DTileMap;
             //this.map.addTilesetImage("tile_floor_forest");
-            this.game.worldMap.addTilesetImage("tile_10");
+            this.game.worldMap.addTilesetImage("tile_floor_dungeon");
+            //this.game.worldMap.addTilesetImage("tile_10");
             this.floorLayer = this.game.worldMap.create('floor', this.mapWidth, this.mapHeight, 16, 16);
-            this.game.worldMap.blockingLayer  = this.game.worldMap.create('blocking', this.mapWidth, this.mapHeight, 16, 16);
+            this.game.worldMap.blockingLayer  = this.game.worldMap.create('blocking', this.mapWidth, this.mapHeight, 16, 16);            
             this.game.worldMap.blockingLayer.key = "blockingLayer";
             this.floorLayer.resizeWorld();
             
@@ -33,7 +34,7 @@ namespace Darkworld.States {
             //fill with grass first
             for (var i = 0; i < randomTileMapData.length; i++) {
                 for (var j = 0; j < randomTileMapData[i].length; j++) {
-                    this.game.worldMap.putTile(0, i, j);//.alpha = 0;;
+                    this.game.worldMap.putTile(this.game.rnd.integerInRange(0,64), i, j);//.alpha = 0;;
                 }
             }
 
@@ -41,13 +42,13 @@ namespace Darkworld.States {
             for (var i = 0; i < randomTileMapData.length; i++) {
                 for (var j = 0; j < randomTileMapData[i].length; j++) {
                     if (randomTileMapData[i][j] == 1) {
-                        var tile = this.game.worldMap.putTile(randomTileMapData[i][j], i, j, this.game.worldMap.blockingLayer);
+                        var tile = this.game.worldMap.putTile(64, i, j, this.game.worldMap.blockingLayer);
                     }
                 }
             }
 
             this.game.worldMap.enableTileMarker();
-            this.game.worldMap.setCollision([1]);
+            this.game.worldMap.setCollision([64]);
             this.game.physics.p2.convertTilemap(this.game.worldMap, this.game.worldMap.blockingLayer);
 
             let openCellPoint:Phaser.Point;
@@ -59,7 +60,7 @@ namespace Darkworld.States {
 
                 
                 let randomTile = this.game.worldMap.getTile(randomX,randomY);
-                console.log(randomX + "-" + randomY + "-"+randomTile.index);
+                
                 if (randomTile.index == 0) {
                     
                     openCellFound = true; //we found an open cell
